f950b5b1ece04892af3262b7b35fedb8cb93f54d
Tanitphon Paniwan <tanit.pani@gmail.com>
2024-06-17 08:57:59 +0700

    remove unused comment

--- app/api/products/route.js
+++ app/api/products/route.js
@@ -1,13 +1,12 @@
 import fsPromises from 'fs/promises'
 
 export async function GET() {
-  const filePath = 'app/api/products/products.json' // Simplified path
+  const filePath = 'app/api/products/products.json'
 
   try {
     const data = await fsPromises.readFile(filePath, 'utf-8')
     const products = JSON.parse(data)
-
-    return Response.json(products) // Streamlined JSON response
+    return Response.json(products)
   } catch (error) {
     console.error('Error reading products.json:', error)
     return new Response('Internal Server Error', { status: 500 })
