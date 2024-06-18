1cbfa17dcf867ea9ac776dd05bf7ce7ddb4f9d92
Tanitphon Paniwan <tanit.pani@gmail.com>
2024-06-18 08:33:12 +0700

    add description with prompt

--- app/api/products/route.js
+++ app/api/products/route.js
@@ -1,11 +1,31 @@
 import fsPromises from 'fs/promises'
+import { geminiText } from '../../utils/gemini'
 
 export async function GET() {
   const filePath = 'app/api/products/products.json'
 
   try {
     const data = await fsPromises.readFile(filePath, 'utf-8')
-    const products = JSON.parse(data)
+    let generateResult = await geminiText(
+      `Your task is to generate concise product descriptions based on the 'name' field in the JSON data provided below. Please follow these guidelines:
+
+      1. Use the 'name' field as inspiration for the description.
+      2. Add a new 'description' field to each product object.
+      3. Return the entire modified JSON array with the new descriptions included.
+
+      **JSON Data:**
+      ${data}
+
+      **Important:** 
+
+      * Ensure the returned response is a strictly valid JSON array.
+      * Do not include any additional text or explanations outside of the JSON structure.
+      `
+    )
+
+    const result = generateResult.replace(/```|json/g, '').trim()
+    console.log('generateResult', result)
+    const products = JSON.parse(result)
     return Response.json(products)
   } catch (error) {
     console.error('Error reading products.json:', error)
