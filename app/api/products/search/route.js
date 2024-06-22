febf18a0a566730806a3a9cb1272b65a53c2b562
Tanitphon Paniwan <tanit.pani@gmail.com>
2024-06-22 11:57:27 +0700

    add python code

---
+++ app/api/products/search/route.js
@@ -0,0 +1,43 @@
+import path from 'path'
+import { fileURLToPath } from 'url'
+import { spawn } from 'child_process'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+export async function GET(request) {
+  // 1. Get Data from API Request
+  const text = request.nextUrl.searchParams.get('text') // Assuming data is passed as a query parameter
+
+  if (!text) {
+    return Response.json({ error: 'Missing text parameter' }, { status: 400 })
+  }
+
+  let newProducts = []
+
+  const pythonProcess = spawn('python3', ['search.py', text], {
+    cwd: __dirname,
+  })
+
+  pythonProcess.stdout.on('data', (data) => {
+    try {
+      const result = data
+        .toString()
+        .replace(/```|json/g, '')
+        .trim()
+      console.log('result', result)
+      newProducts = JSON.parse(result)
+    } catch (error) {
+      console.error('Error parsing JSON:', error)
+    }
+  })
+
+  pythonProcess.stderr.on('data', (data) => {
+    console.error(`Python script error: ${data}`)
+  })
+
+  await new Promise((resolve) => {
+    pythonProcess.on('close', resolve)
+  })
+  return Response.json(newProducts)
+}
